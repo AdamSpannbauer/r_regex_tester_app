c402f275923038fba1420b347c65198252302313
Adam Spannbauer <SpannbauerAdam@gmail.com>
2017-05-08 16:57:16 -0400

    v0 regexplainer func

--- regexplainer.R
+++ regexplainer.R
@@ -6,7 +6,8 @@ regexplain <- function(regx) {
   # regx <- '\\s+\\s+'
   base_url <- "http://rick.measham.id.au/paste/explain?regex="
   regx_url <- URLencode(regx) %>% 
-    stringr::str_replace_all(stringr::fixed("+"), "%2B")
+    stringr::str_replace_all(stringr::fixed("+"), "%2B") %>% 
+    stringr::str_replace_all(fixed('[[:blank:]]'), '[:blank:]')
   
   full_url <- paste0(base_url, regx_url)
   
@@ -37,15 +38,19 @@ regexplain <- function(regx) {
     purrr::map(~c(.x[1], paste(.x[-1], collapse=" ")) %>% 
                  trimws()) %>% 
     magrittr::set_names(paste0("node_", seq_along(.)))
-  clean_row_list
-  # explain_df <- do.call('rbind', clean_row_list) %>% 
-  #   dplyr::as_data_frame() %>% 
-  #   magrittr::set_names(c("regex","explanation"))
-  # explain_df
+  # clean_row_list
+  explain_df <- do.call('rbind', clean_row_list) %>%
+    dplyr::as_data_frame() %>%
+    magrittr::set_names(c("regex","explanation"))
+  
+  if(stringr::str_detect(regx, fixed('[[:blank:]]')) & 
+     any(stringr::str_detect(explain_df$regex,fixed('[:blank:]')))) {
+    blank_inds <- which(explain_df$regex == '[:blank:]')
+    explain_df$regex[blank_inds] <- '[[:blank:]]'
+    explain_df$explanation[blank_inds] <- 'any character of: blank characters (space and tab, and possibly other locale-dependent characters such as non-breaking space)'
+  }
+  
+  explain_df
 }
 
-regexplain("\\s+.")
-
-
-
-
+View(regexplain("\\s+.[[:blank:]][[:alpha:]]"))
