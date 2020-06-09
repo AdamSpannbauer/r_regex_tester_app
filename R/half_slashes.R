0b4cc83627e7f5ab46f6af64f83d42a8e61a2094
AdamSpannbauer <spannbaueradam@gmail.com>
2020-06-09 07:06:37 -0400

    cvt to golem pkg

---
+++ R/half_slashes.R
@@ -0,0 +1,52 @@
+half_slashes = function(str, exclude = character(0)) {
+  #' Given a string, remove half of the escaping backslashes
+  #'
+  #' @param str A string to remove backslashes from
+  #' @param exclude A character vector of items to exclude from processing
+  #'
+  #' @return A version of the input string with half of the escaping chars
+  #' @keywords internal
+  #' @importFrom data.table :=
+  #'
+  #' @details An exception will be thrown if removing slashes leads to a parsing
+  #'         error.  For example, "\\" will be converted to "\" which is not
+  #'         a valid string.
+  #'
+  #' @examples
+  #' # half_slashes("\\\\")
+  #' # [1] "\\"
+
+  # Satisfy global variable check issues w/o globalVariables
+  # These are col names used in NSE data.table expressions
+  half_slash = NULL
+  slash_cap = NULL
+  out = NULL
+  char_cap = NULL
+
+  deparsed = deparse(str)
+
+  half_df = data.table::as.data.table(
+    stringr::str_match_all(deparsed, "(\\\\)(.)")[[1]]
+  )
+
+  data.table::setnames(half_df, c("match", "slash_cap", "char_cap"))
+  half_df[, half_slash := stringr::str_sub(slash_cap,
+                                           end = (nchar(slash_cap) / 2))]
+  half_df[, out := paste0(half_slash, char_cap)]
+  half_df = unique(half_df[order(nchar(out)), ])
+
+  # Removing slashes before double quotes breaks eval
+  half_df = half_df[char_cap != '"']
+
+  # Dirty fix for new lines...
+  for (x in exclude) {
+    half_df[match == x, out := x]
+  }
+
+  halfed_deparse = mgsub(half_df$match,
+                         half_df$out,
+                         deparsed,
+                         order.pattern = FALSE)
+
+  eval(parse(text = halfed_deparse))
+}
---
+++ R/try_default.R
@@ -0,0 +1,24 @@
+try_default = function(fun, default = NULL, silent = FALSE) {
+  #' Create a 'safe' version of a function with a default return value
+  #'
+  #' @param fun function to add try logic to
+  #' @param default default value to return if fun errors
+  #' @param silent should error be printed to console?
+  #'
+  #' @return either output of fun or default value in case of error
+  #' @keywords internal
+  #'
+  #' @examples
+  #' # add = function(a, b) a + b
+  #' # default_add = try_default(add)
+  #' # null_output = default_add(1, 'a')
+
+  function(...) {
+    out = default
+    try({
+      out = fun(...)
+    }, silent = silent)
+
+    return(out)
+  }
+}
