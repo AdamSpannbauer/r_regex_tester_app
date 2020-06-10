875aa6322a972540b0751d5530854a3c8fb52869
AdamSpannbauer <spannbaueradam@gmail.com>
2020-06-10 09:04:49 -0400

    fix app title

--- R/app_ui.R
+++ R/app_ui.R
@@ -192,7 +192,7 @@ golem_add_external_resources <- function() {
     golem::favicon(ext = "png"),
     golem::bundle_resources(
       path = app_sys("app/www"),
-      app_title = "r_regex_tester_appg"
+      app_title = "R Regex Tester"
     )
   )
 }
